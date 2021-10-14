bccfb1abf83e0e709c51fd0fef518939635bd6b4
Entrar <Entrar@PC-1122>
2021-10-14 09:55:52 +0100

    Se ha creado una nueva clase genérica Histogram<T>.

--- src/kata3_42/Kata3_42.java
+++ src/kata3_42/Kata3_42.java
@@ -15,8 +15,19 @@ public class Kata3_42 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HistogramDisplay histo = new HistogramDisplay();
-        histo.execute();
+        Histogram<String> histogram = new Histogram();
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        new HistogramDisplay(histogram).execute();
     }
     
 }
